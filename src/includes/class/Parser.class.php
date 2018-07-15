02ed78d0052d2070826a9aaa0456c514ad8d6b1c
Matthijs Brouwer <matthijs@brouwer.info>
2018-07-15 17:45:28 +0200

    disable facet.range.mincount (solr-problem sharding)

--- src/includes/class/Parser.class.php
+++ src/includes/class/Parser.class.php
@@ -1078,14 +1078,15 @@ class Parser {
               $this->warnings [] = "facets - facetranges - ex should be a string";
             }
           }
-          if (isset ( $object->mincount )) {
-            $ignoreList [] = "mincount";
-            if (is_numeric ( $object->mincount )) {
-              $object->__options [] = "facet.mincount=".$object->mincount;
-            } else {
-              $this->warnings [] = "facets - facetranges - mincount should be numeric";
-            }
-          }
+// disabled: solr-problem with sharding
+//           if (isset ( $object->mincount )) {
+//             $ignoreList [] = "mincount";
+//             if (is_numeric ( $object->mincount )) {
+//               $object->__options [] = "facet.mincount=".$object->mincount;
+//             } else {
+//               $this->warnings [] = "facets - facetranges - mincount should be numeric";
+//             }
+//           }
           if (isset ( $object->key )) {
             if (is_string ( $object->key )) {
               $counter = 0;
