f9a4db5681e06469b9ebecbb2cbb50dd3f2910e0
LB <lhuygi@users.noreply.github.com>
2018-09-21 10:45:19 +0200

    Change Dockerfile to run code in this repo, not from the web

---
+++ docker/start.sh
@@ -0,0 +1,7 @@
+echo
+echo "=============================== BROKER ================================"
+echo "  See https://meertensinstituut.github.io/broker/ for more information"
+echo "======================================================================="
+echo
+
+exec service apache2 start
